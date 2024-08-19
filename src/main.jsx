3ddf4708810855cc7c37d18a01731fad079bfbf5
Omerf130 <ofaclieru@gmail.com>
2024-08-19 21:30:30 +0300

    app router structure created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.jsx';
+import { Home } from './pages/home/Home.jsx';
+import { Inbox } from './pages/inbox/Inbox.jsx';
+import { Sent } from './pages/sent/Sent.jsx';
+import { Starred } from './pages/starred/Starred.jsx';
+import { Trash } from './pages/trash/Trash.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/inbox",
+        element: <Inbox/>,
+      },
+      {
+        path: "/sent",
+        element: <Sent/>,
+      },
+      {
+        path: "/starred",
+        element: <Starred/>,
+      },
+      {
+        path: "/trash",
+        element: <Trash/>,
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
